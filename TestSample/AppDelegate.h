f5ae4570d6f94ce2651a93940de6aab17ca1a71b
violet deng <dengviolet@gmail.com>
2019-12-20 11:23:20 +0800

    Initial Commit

---
+++ TestSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestSample
+//
+//  Created by Violet Deng on 2019/12/20.
+//  Copyright © 2019 Violet Deng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TestSample/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  TestSample
+//
+//  Created by Violet Deng on 2019/12/20.
+//  Copyright © 2019 Violet Deng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
